b2834c3738d721ce735605d49e5d0bcc32c6c335
Taras Shchur <shchurtaras@hotmail.com>
2019-12-05 15:09:17 +0200

    reimplement mobx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+
+let accsessedProperties = [];
+const derivationGraph = {};
+
+function observable(targetObject) {
+  const ObservableObject = {};
+
+  const keys = Object.keys(targetObject);
+
+  const id = Math.random();
+
+  function getId(key) {
+    return `Observable(${id}.${key})`;
+  };
+
+  keys.forEach(key => {
+    const id = getId(key);
+
+    ObservableObject[key] = targetObject[key];
+
+    if (typeof targetObject[key] !== "function") {
+      Object.defineProperty(ObservableObject, key, {
+        get() {
+          accsessedProperties.push(id);
+          return targetObject[key];
+        },
+
+        set(val) {
+          targetObject[key] = val;
+
+          if (derivationGraph[id]) {
+            derivationGraph[id].forEach(fn => {
+              fn();
+            })
+          }
+        }
+      })
+    }
+  });
+
+  return ObservableObject;
+}
+
+function createReaction(whatShouldWeRunOnChange) {
+  return {
+    track(functionWhereWeUseObservables) {
+      accsessedProperties = [];
+      functionWhereWeUseObservables();
+
+      accsessedProperties.forEach((id) => {
+        derivationGraph[id] = derivationGraph[id] || [];
+
+        if (derivationGraph[id].indexOf(whatShouldWeRunOnChange) < 0) {
+          derivationGraph[id].push(whatShouldWeRunOnChange);
+        }
+      })
+    }
+  }
+}
+
+function autorun(cb) {
+  const reaction = createReaction(cb);
+
+  reaction.track(cb);
+}
+
+function UseForceUpdate() {
+  const [, set] = useState(0);
+
+  return () => set(val => val + 1);
+}
+
+function observer(baseComponent) {
+  const Wrapper = () => {
+    const forceUpdate = UseForceUpdate();
+    const reaction = useRef(null);
+
+    if (!reaction.current) {
+      reaction.current = createReaction(forceUpdate);
+    }
+
+    let result;
+
+    reaction.current.track(() => {
+      result = baseComponent();
+    });
+
+    return result;
+  };
+
+  return Wrapper;
+}
+
+const store = observable({
+  count: 0,
+  somethingElse: 0,
+
+  increment() {
+    this.count += 1;
+  }
+});
+
+autorun(() => {
+  console.log("count autorun", store.count);
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <h1>Counter {store.count}</h1>
+        <button onClick={() => store.increment()} />
+      </div>
+    )
 }
 
-export default App;
+const ObserverApp = observer(App);
+
+export default ObserverApp;
